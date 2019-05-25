9ab8950f85b684d2882dd46546214137941e0ad7
John Liu <johnliu55tw@gmail.com>
2019-05-25 23:50:08 +0800

    Add configurations for Elegant theme
    Ref: https://pelican-elegant.github.io/configuration-variables-and-metadata-list

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,8 +2,7 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-PLUGIN_PATHS = ['./plugins']
-PLUGINS = ['pelican_cjk']
+PLUGIN_PATHS = ['./plugins', './pelican-plugins']
 
 THEME = 'themes/elegant'
 AUTHOR = 'John Liu'
@@ -17,6 +16,16 @@ PATH = 'content'
 TIMEZONE = 'Asia/Taipei'
 
 DEFAULT_LANG = 'zh-tw'
+
+# Elegant Theme
+# https://pelican-elegant.github.io/configuration-variables-and-metadata-list
+TAG_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+STATIC_PATHS = ['theme/images', 'images']
+PLUGINS = ['pelican_cjk', 'sitemap', 'tipue_search', 'neighbors']
+DIRECT_TEMPLATES = ('index', 'tags', 'categories', 'archives', 'search', '404')
+SITEMAP = {'format': 'xml'}
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
